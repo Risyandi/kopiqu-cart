99385093a6ba31cab4ffd26b49c12557e8ff05a8
Risyandi <risyandi.94@gmail.com>
2022-03-24 22:42:32 +0700

    fix: update styles Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,15 +10,20 @@ class Home extends Component{
     render(){
         let itemList = this.props.items.map(item=>{
             return(
-                <div className="card" key={item.id}>
+                <div className="card pink lighten-5 hoverable" key={item.id}>
                         <div className="card-image">
                             <img src={item.img} alt={item.title}/>
-                            <span className="card-title">{item.title}</span>
-                            <span to="/" className="btn-floating halfway-fab waves-effect waves-light red" onClick={()=>{this.handleClick(item.id)}}><i className="material-icons">add</i></span>
+                            <span to="/" className="btn-floating halfway-fab waves-effect waves-light red pulse" onClick={()=>{this.handleClick(item.id)}}><i className="material-icons">add</i></span>
                         </div>
-
                         <div className="card-content">
+                                <span className="card-title">
+                                    <strong>
+                                    {item.title}
+                                    </strong>
+                                </span>
                             <p>{item.desc}</p>
+                        </div>
+                        <div className="card-content">
                             <p><b>Price: Rp. {item.price}K</b></p>
                         </div>
                  </div>
@@ -48,4 +53,4 @@ const mapDispatchToProps= (dispatch)=>{
     }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Home)
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
