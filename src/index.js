30c8132d9c62fee7efa0df090a59af55c7d5b884
Risyandi <risyandi.94@gmail.com>
2022-03-24 22:43:31 +0700

    fix: update index

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import cartReducer from './components/reducers/cartReducer';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 
 const store = createStore(cartReducer);
+console.log(store, "debug: store index.js");
 
 ReactDOM.render(
-    <Provider store={store}><App/></Provider>, document.getElementById('root'));
+    <Provider store={store}><App/></Provider>, document.getElementById('root')
+);
